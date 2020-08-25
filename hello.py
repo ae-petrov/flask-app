77f5888ac53f9779249e9310677983bbdf973e78
Алексей Петров <foto.petrovalexei@yandex.ru>
2020-08-25 14:08:21 +0300

    app is ready

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask inmport Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Got it!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
